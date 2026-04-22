39907821ba75210ca56b770eb568fafc114b4254
bunnyrdy <121040061+bunnyrdy@users.noreply.github.com>
2026-04-22 21:54:01 +0530

    terraform

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "eks" {
   key_name   = "expense-eks"
-  public_key = file(pathexpand("~/.ssh/eks.pub"))
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDYKXdraPFv8U1gyd0ELNNqy+ZntsC+4csub3kSsh2N2dGysRoH/C0lhIzWEBWrwjl+BeqUv9Yk92hKjfJ578RMjoXrx4eZzbuC9KY7wGExmNuiRWBQSJRPITyZ+4CphODtQraanlXhFYIi/uwjKIPiT+edknxTNA72GVexOwEOP5GMiGprvB2s0Zw7cNEnmhrV22+IZFbJFbPUQ2FPqi79Llg+7jqoFATOebWv2wXstZR2/4VEfYlx9pS02OH5+IdKiBIvaeZMnJSWsBHykXTqBZ1EefIZI9RLphkqmIl9cWatMQl378CG+yJq7+LCKDnRZmYVDJHkwdI91wtei6cuv6mzeTyXkMjNycA1Ef5GAM7OblgI7w0usXImnwuHlInQYFzkY2z1jwhzBl16x+ONwgWRULy/XejsgI8rn9rzEB3tyyx5rBSE4/MB9NIms1W5rOWqZaM53co/1t+GIvxTn7wLkn02MH8o7MB22wE1rG03Pju9YvVqV0zzU4iVsqmROq9UsvQcmudX57UkCYSxmfpA9ebBjMkNi7TCkxjgoZcG5OWexGExnUwPm1uke7MkTQ+pYANDH9LvdNXRb4i6IS84QKEpXDDGi30xi/e45J5PblSZ3K01MJE8KKwzVVtLc8Iwodf66OwX+5pbWhdaZpnwkogiz1FgsAg0yNJ3Fw== srinivasreddy@Srinivass-MacBook-Air.local"
 }
 
 module "eks" {
@@ -39,25 +39,9 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    /* blue = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      #ami_type       = "AL2_x86_64"
-      instance_types = ["m5.xlarge"]
-      key_name = aws_key_pair.eks.key_name
-
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
-      iam_role_additional_policies = {
-        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-        AmazonEFSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        AmazonEKSLoadBalancingPolicy = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
-      }
-    } */
-
-    green = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      #ami_type       = "AL2_x86_64"
+    blue = {
+      //Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2_x86_64"
       instance_types = ["t3.small"]
       key_name = aws_key_pair.eks.key_name
 
@@ -69,7 +53,23 @@ module "eks" {
         AmazonEFSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
         AmazonEKSLoadBalancingPolicy = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
       }
-    }
+    } 
+
+    # green = {
+    #   # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+    #   #ami_type       = "AL2_x86_64"
+    #   instance_types = ["t3.small"]
+    #   key_name = aws_key_pair.eks.key_name
+
+    #   min_size     = 2
+    #   max_size     = 10
+    #   desired_size = 3
+    #   iam_role_additional_policies = {
+    #     AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+    #     AmazonEFSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+    #     AmazonEKSLoadBalancingPolicy = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
+    #   }
+    # }
   }
 
   tags = merge(
